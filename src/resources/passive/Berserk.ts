a1a20a1e81030210f10f2c60978622611543ade1
Plinkie03 <rubenellolkito@gmail.com>
2025-01-11 16:28:38 +0100

    fix(Berserk): Listen for spell attacks

--- src/resources/passive/Berserk.ts
+++ src/resources/passive/Berserk.ts
@@ -1,4 +1,5 @@
 import { Hit } from "../../structures/battle/actions/Hit.js";
+import { SpellAttack } from "../../structures/battle/actions/SpellAttack.js";
 import { ItemPassive } from "../../structures/resource/ItemPassive.js";
 import { Util } from "../../structures/static/Util.js";
 
@@ -7,10 +8,10 @@ const MaxMultiplier = 0.3
 export default new ItemPassive({
     id: 1,
     name: "Berserk",
-    info: payload => `For every percent of health lost, Damage Taken -1%, Strength +1% (Caps at ${Util.formatFloat(MaxMultiplier * 1e2)}%)`,
-    criteria: payload => payload.action instanceof Hit,
+    info: payload => `For every percent of health lost, Damage Taken -1%, Strength +1% (Caps at ${Util.formatFloat(MaxMultiplier * 1e2)})`,
+    criteria: payload => payload.action instanceof Hit || payload.action instanceof SpellAttack,
     execute(payload) {
-        const hit = payload.action as Hit
+        const hit = payload.action as Hit | SpellAttack
         const hpLostMultiplier = Math.min(1 - payload.entity.hp / payload.entity.moddedStats.maxHealth, MaxMultiplier)
 
         if (hit.entity === payload.entity) {
